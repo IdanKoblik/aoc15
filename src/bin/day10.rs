80756298d41db9752470377aca98191a2b326077
Idan Koblik <Idan Koblik>
2026-06-24 11:03:32 +0300

    day10

---
+++ src/bin/day10.rs
@@ -0,0 +1,41 @@
+fn main() {
+    let mut input = String::from("1321131112");
+
+    for i in 0..50 {
+        let mut target = None;
+        let mut count = 0;
+        let mut result = String::new();
+
+        for c in input.chars() {
+            if target.is_none() {
+                target = Some(c);
+                count += 1;
+                continue;
+            }
+
+            if c == target.unwrap() {
+                count += 1;
+                continue;
+            }
+
+            if let Some(ch) = target {
+                result += &format!("{count}{ch}");
+            }
+            target = Some(c);
+            count = 1;
+        }
+
+        if let Some(ch) = target {
+            result += &format!("{count}{ch}");
+        }
+
+        input = result;
+        let len = input.len();
+
+        match i {
+            39 => println!("Part 1: {len}"),
+            49 => println!("Part 2: {len}"),
+            _ => {}
+        }
+    }
+}
